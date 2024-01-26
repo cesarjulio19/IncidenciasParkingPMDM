5ef50210951f79efaeb300ff251204dc5703d048
Boorja21 <115183112+boorjabraavo21@users.noreply.github.com>
2024-01-26 09:48:44 +0100

    Login creado y probando fragmento Incidencias

--- app/src/main/java/com/example/incidenciasparkingpmdm/MainActivity.kt
+++ app/src/main/java/com/example/incidenciasparkingpmdm/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.incidenciasparkingpmdm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.GravityCompat
+import com.example.incidenciasparkingpmdm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.botoncillo.setOnClickListener {
+            binding.drawerLayout.openDrawer(GravityCompat.START)
+        }
     }
 }
